2c98a56baa0fb0d79f10706a11cda60be6fe3dec
ToanJt <lexuantoan5703@gmail.com>
2026-03-06 14:59:37 +0700

    update

--- medusa-config.ts
+++ medusa-config.ts
@@ -7,9 +7,15 @@ module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      storeCors:
+        process.env.STORE_CORS ||
+        "http://localhost:3000,https://moonlit-cupcake-1a9c76.netlify.app",
+      adminCors:
+        process.env.ADMIN_CORS ||
+        "http://localhost:3000,https://moonlit-cupcake-1a9c76.netlify.app",
+      authCors:
+        process.env.AUTH_CORS ||
+        "http://localhost:3000,https://moonlit-cupcake-1a9c76.netlify.app",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
