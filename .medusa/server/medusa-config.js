4307df29286d4f7d1f67bd0340219edc5c896303
ToanJt <lexuantoan5703@gmail.com>
2026-03-06 07:41:13 +0700

    fix

--- .medusa/server/medusa-config.js
+++ .medusa/server/medusa-config.js
@@ -6,12 +6,12 @@ module.exports = (0, utils_1.defineConfig)({
     projectConfig: {
         databaseUrl: process.env.DATABASE_URL,
         http: {
-            storeCors: "http://localhost:3000,http://169.254.82.84:3000,https://moonlit-cupcake-1a9c76.netlify.app",
-            adminCors: "http://localhost:9000,http://169.254.82.84:3000,https://moonlit-cupcake-1a9c76.netlify.app",
-            authCors: "http://localhost:3000,http://169.254.82.84:3000,https://moonlit-cupcake-1a9c76.netlify.app",
+            storeCors: "http://localhost:3000,https://moonlit-cupcake-1a9c76.netlify.app",
+            adminCors: "inshoo-backend-production.up.railway.app,https://moonlit-cupcake-1a9c76.netlify.app",
+            authCors: "http://localhost:3000,https://moonlit-cupcake-1a9c76.netlify.app",
             jwtSecret: process.env.JWT_SECRET || "supersecret",
             cookieSecret: process.env.COOKIE_SECRET || "supersecret",
         },
     },
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWVkdXNhLWNvbmZpZy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL21lZHVzYS1jb25maWcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxxREFBa0U7QUFFbEUsSUFBQSxlQUFPLEVBQUMsT0FBTyxDQUFDLEdBQUcsQ0FBQyxRQUFRLElBQUksYUFBYSxFQUFFLE9BQU8sQ0FBQyxHQUFHLEVBQUUsQ0FBQyxDQUFDO0FBRTlELE1BQU0sQ0FBQyxPQUFPLEdBQUcsSUFBQSxvQkFBWSxFQUFDO0lBQzVCLGFBQWEsRUFBRTtRQUNiLFdBQVcsRUFBRSxPQUFPLENBQUMsR0FBRyxDQUFDLFlBQVk7UUFDckMsSUFBSSxFQUFFO1lBQ0osU0FBUyxFQUNQLDRGQUE0RjtZQUM5RixTQUFTLEVBQ1AsNEZBQTRGO1lBQzlGLFFBQVEsRUFDTiw0RkFBNEY7WUFDOUYsU0FBUyxFQUFFLE9BQU8sQ0FBQyxHQUFHLENBQUMsVUFBVSxJQUFJLGFBQWE7WUFDbEQsWUFBWSxFQUFFLE9BQU8sQ0FBQyxHQUFHLENBQUMsYUFBYSxJQUFJLGFBQWE7U0FDekQ7S0FDRjtDQUNGLENBQUMsQ0FBQyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWVkdXNhLWNvbmZpZy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL21lZHVzYS1jb25maWcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxxREFBa0U7QUFFbEUsSUFBQSxlQUFPLEVBQUMsT0FBTyxDQUFDLEdBQUcsQ0FBQyxRQUFRLElBQUksYUFBYSxFQUFFLE9BQU8sQ0FBQyxHQUFHLEVBQUUsQ0FBQyxDQUFDO0FBRTlELE1BQU0sQ0FBQyxPQUFPLEdBQUcsSUFBQSxvQkFBWSxFQUFDO0lBQzVCLGFBQWEsRUFBRTtRQUNiLFdBQVcsRUFBRSxPQUFPLENBQUMsR0FBRyxDQUFDLFlBQVk7UUFDckMsSUFBSSxFQUFFO1lBQ0osU0FBUyxFQUNQLGtFQUFrRTtZQUNwRSxTQUFTLEVBQ1AscUZBQXFGO1lBQ3ZGLFFBQVEsRUFDTixrRUFBa0U7WUFFcEUsU0FBUyxFQUFFLE9BQU8sQ0FBQyxHQUFHLENBQUMsVUFBVSxJQUFJLGFBQWE7WUFDbEQsWUFBWSxFQUFFLE9BQU8sQ0FBQyxHQUFHLENBQUMsYUFBYSxJQUFJLGFBQWE7U0FDekQ7S0FDRjtDQUNGLENBQUMsQ0FBQyJ9
